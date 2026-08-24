8588fe3c9bb749d8fdc395487b32b3ceda3b3a85
Emirhan <emirhan.bda0@gmail.com>
2026-08-24 14:54:16 +0300

    s

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { User, UserSchema } from '../users/schema/user.schema'
 
 @Module({
+  imports: [
+   MongooseModule.forFeature([{name:User.name,schema:UserSchema}]),
+   JwtModule.register(
+    {
+        secret:process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRES_IN as any},
+      },
+   ),
+   PassportModule.register({defaultStrategy:'jwt'})
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,JwtStrategy,],
+  exports:[JwtStrategy,PassportModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,118 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register-dto';
 import { LoginDto } from './dto/login-dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 import { ForgotPasswordDto } from './dto/forgot-password-dto';
 import { ResetPasswordDto } from './dto/reset-password-dto';
-import { VerifyEmailDto } from './dto/verify-email-dto'
-
+import { VerifyEmailDto } from './dto/verify-email-dto';
+import { MailService } from './mail/mail.service';
 
 @Injectable()
 export class AuthService {
-  constructor
+  constructor(
+    @InjectModel('User')
+    private userModel: Model<any>,
+    private jwtService: JwtService,
+    private mailService: MailService,
+  ) {}
+
+  async register(registerDto: RegisterDto) {
+    if (registerDto.password != registerDto.passwordConfirm) {
+      throw new BadRequestException('Şifreler birbiriyle eşleşmiyor!');
+    }
+    const existingUser = await this.userModel.findOne({
+      email: registerDto.email,
+    });
+    if (existingUser) {
+      throw new BadRequestException('Bu e-posta adresi zaten kullanımda!');
+    }
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    const verificationCode = Math.floor(
+      100000 + Math.random() * 900000,
+    ).toString();
+    const newUser = new this.userModel({
+      name: registerDto.name,
+      email: registerDto.email,
+      password: hashedPassword,
+      verificationCode: verificationCode,
+    });
+    await newUser.save();
+    await this.mailService.sendVerificationEmail(
+      registerDto.email,
+      verificationCode,
+    );
+    return {
+      message:
+        'Kayıt başarılı. Lütfen e-postanıza gönderilen kod ile hesabınızı doğrulayın.',
+    };
+  }
+  async login(loginDto : LoginDto){
+     const user = await this.userModel.findOne({email:loginDto.email})
+    if(!user){
+      throw new UnauthorizedException("E-posta veya şifre hatalı!")
+    }
+    const isPasswordValid = await bcrypt.compare(loginDto.password, user.password); 
+    if(!isPasswordValid){
+      throw new UnauthorizedException("E-posta veya şifre hatalı!")
+    }
+    const payload = {sub: user._id, email: user.email}
+    const generatedCode = await this.jwtService.signAsync(payload)
+    return {access_token : generatedCode}
+  }
+  async forgotPassword(forgotPasswordDto:ForgotPasswordDto){
+    const user = await this.userModel.findOne({email:forgotPasswordDto.email})
+    if(!user){
+      throw new NotFoundException("Böyle bir kullanıcı bulunamadı!")
+    }
+    const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const expireDate = new Date(Date.now() + 15 * 60 * 1000)
+
+    user.resetPasswordCode = resetCode;
+    user.resetPasswordExpires = expireDate;
+    await user.save();
+    await this.mailService.sendPasswordResetEmail(forgotPasswordDto.email,resetCode )
+    return {message : 'Şifre sıfırlama kodu başarıyla e-postanıza gönderildi.'};  
+  }
+  async resetPassword(resetPasswordDto:ResetPasswordDto){
+    if(resetPasswordDto.newPassword != resetPasswordDto.newPasswordConfirm){
+      throw new BadRequestException("Şifreler birbiriyle eşleşmiyor!")
+    }
+    const user = await this.userModel.findOne({
+      email:resetPasswordDto.email,
+      resetPasswordCode:resetPasswordDto.code,
+      resetPasswordExpires:{$gt: new Date()}
+    })
+    if(!user){
+     throw new BadRequestException("Geçersiz veya süresi dolmuş kod!")
+    }
+    const newHashPassword = await bcrypt.hash(resetPasswordDto.newPassword,10)
+    user.password = newHashPassword;
+    user.resetPasswordCode = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+    return {message: "Şifreniz başarıyla değiştirildi."}
+  }
+  async verifyEmail(verifyEmailDto: VerifyEmailDto){
+     const user = await this.userModel.findOne({
+      email:verifyEmailDto.email,
+      verificationCode:verifyEmailDto.code,
+      verificationExpires: { $gt: new Date() }
+    })
+    if(!user){
+      throw new BadRequestException("Geçersiz veya süresi dolmuş kod!")
+    }
+    user.isVerified = true;
+    user.verificationCode = undefined;
+    user.verificationExpires = undefined;
+    await user.save();
+    return {message:"Hesabınız başarıyla doğrulandı."}
+  }
 }
