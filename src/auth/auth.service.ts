996dd81a048663ee3bddc74356070dc40dd89b84
Emirhan <emirhan.bda0@gmail.com>
2026-08-24 14:31:18 +0300

    s

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { RegisterDto } from './dto/register-dto';
+import { LoginDto } from './dto/login-dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt'
+import { ForgotPasswordDto } from './dto/forgot-password-dto';
+import { ResetPasswordDto } from './dto/reset-password-dto';
+import { VerifyEmailDto } from './dto/verify-email-dto'
+
+
 @Injectable()
 export class AuthService {
-  
+  constructor
 }
