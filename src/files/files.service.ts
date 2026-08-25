96edd619c960428df0725bb08db4264ce928b51e
Emirhan <emirhan.bda0@gmail.com>
2026-08-25 15:11:48 +0300

    s

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class FilesService {
-  
+  constructor(
+    @InjectModel('File') 
+    private fileModel: Model<any>,
+  ) {}
+  async createFile(file: Express.Multer.File, folderId: string, ownerId: string) {
+    const newFile = await this.fileModel.create({
+      originalName: file.originalname, 
+      fileName: file.filename,       
+      mimeType: file.mimetype,    
+      size: file.size,
+      ownerId: ownerId,       
+      folderId: folderId || null,
+    });
+    return newFile;
+  }
 }
