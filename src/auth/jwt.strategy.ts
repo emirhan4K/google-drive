b532ecb5cbab0cfe10723654e6261a1aa0016085
Emirhan <emirhan.bda0@gmail.com>
2026-09-01 16:17:21 +0300

    d

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,17 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Redis } from 'ioredis';
+import { Request } from 'express';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @Inject('REDIS_CLIENT') private readonly redisClient :Redis,
+  ) {
     super({
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //Gelen http isteklerinin neresine bakacak
-        secretOrKey: process.env.JWT_SECRET as string //Aldığı tokenın sahte olup olmadığını kontrol et
+        ignoreExpiration: false, 
+        secretOrKey: process.env.JWT_SECRET as string, //Aldığı tokenın sahte olup olmadığını kontrol et
+        passReqToCallback: true, //Gelen token metnini görebilmesi için (Req) i paslar
     });
   }
 
-  async validate(payload: any) { //Token gerçek ve süresi dolmamışsa fonk otomatik çalışacak 
-    return {id: payload.sub, email: payload.email} //Payload içindeki verileri alıp dışarı  obje olarak aktarıyoruz.
+  async validate(request:Request,payload: any) { //User içeri alınmadan önce çalışır
+    const authHeader = request.headers.authorization as string;
+    const token = authHeader.split(' ')[1];
+     
+    const isBlacklisted = await this.redisClient.get(`blacklist:${token}`); //Redise soruyoruz bu token kara listede var mı ?
+    if(isBlacklisted){
+      throw new UnauthorizedException('Bu oturum sonlandırılmış! Lütfen tekrar giriş yapınız.')
+    }
+    return {userId:payload.sub, email:payload.email} //Token kara listede değilse içeri girmesine izin veriyoruz.
   }
 }
