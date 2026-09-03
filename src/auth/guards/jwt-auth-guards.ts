1645ba151f6c21600baf5e109e47e368b56d70df
Emirhan <emirhan.bda0@gmail.com>
2026-09-03 14:11:35 +0300

    s

--- src/auth/guards/jwt-auth-guards.ts
+++ src/auth/guards/jwt-auth-guards.ts
@@ -1,15 +1,18 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Reflector } from "@nestjs/core"; // SetMetadata ile yapıştırılan etiketleri  okur
 import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
+import {Redis} from "ioredis";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt'){
-    constructor(private reflector:Reflector){
-        super()
+    constructor(
+        private reflector:Reflector,
+        @Inject('REDIS-CLIENT') private readonly redisClient: Redis
+    ) {
+        super();
     }
-
-    canActivate(context: ExecutionContext){ 
+    async canActivate(context: ExecutionContext): Promise<boolean> { //Geriye bir değer döndüreceğiz 
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,[
             context.getHandler(),//Fonksiyonlara bak
             context.getClass()//Controllera bak 
@@ -17,7 +20,26 @@ export class JwtAuthGuard extends AuthGuard('jwt'){
         if (isPublic) {
       return true;
     }
-    return super.canActivate(context);
+    //Passport kontrolü yapar ve token geçerli mi diye bakar
+    const isValid = await super.canActivate(context);
+    if (!isValid) {
+        return false;   
     }
-    
+
+    //Token geçerli ise, tokeni alır ve redis'te blacklistte olup olmadığını kontrol eder
+    const request = context.switchToHttp().getRequest(); //Request objesini alır
+    const token = this.extractTokenFromHeader(request); //Headerdan gelen saf tokenı alır ve token değişkenine atar
+    if (token) {
+            const isBlacklisted = await this.redisClient.get(`blacklist:${token}`);
+            if (isBlacklisted) {
+                throw new UnauthorizedException('Oturumunuz kapatılmış. Lütfen tekrar giriş yapın!');
+            }
+        }
+        return true;
+    }
+    //Header'dan (Authorization: Bearer <token>) sadece token kısmını koparıp alan yardımcı metod.
+    private extractTokenFromHeader(request: any): string | undefined { //Request objesinden tokeni alır
+        const [type, token] = request.headers.authorization?.split(' ') ?? []; //Header'dan Bearer ve tokeni ayırır
+        return type === 'Bearer' ? token : undefined; //Eğer Bearer ise tokeni döndürür, değilse undefined döndürür
+    }
 }
