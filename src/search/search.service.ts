432af7bbc8b1a43e57aea041b3326d5f918820c5
Emirhan <emirhan.bda0@gmail.com>
2026-09-15 15:33:05 +0300

    s

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -4,7 +4,7 @@ import { createClient, RedisClientType } from 'redis';
 @Injectable()
 export class SearchService implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(SearchService.name);
-  private redisClient: RedisClientType;
+  private redisClient: RedisClientType; //Otomatik tamamlama sağlar
 
   constructor() {
     this.redisClient = createClient({
@@ -14,13 +14,13 @@ export class SearchService implements OnModuleInit, OnModuleDestroy {
     this.redisClient.on('error', (err) => this.logger.error('Redis Bağlantı Hatası:', err));
   }
 
-  async onModuleInit() {
+  async onModuleInit() { //Proje başladığında ilk çalışacak kısım
     await this.redisClient.connect();
     this.logger.log('RediSearch motoruna başarıyla bağlanıldı! 🚀');
     await this.createFileIndex();
   }
 
-  async onModuleDestroy() {
+  async onModuleDestroy() { //Sunucuyu durdurduğumuzda devreye girer ve açık kalan redis bağlantılarını kapatır.
     if (this.redisClient) {
       await this.redisClient.quit();
     }
@@ -32,16 +32,16 @@ export class SearchService implements OnModuleInit, OnModuleDestroy {
         '$.name': { type: 'TEXT', AS: 'name', sortable: true },
         '$.extension': { type: 'TAG', AS: 'extension' },
         '$.size': { type: 'NUMERIC', AS: 'size' }
-      };
-      await this.redisClient.ft.create('idx:files', searchSchema as any, {
-        ON: 'JSON',
-        PREFIX: 'file:' 
+      };    
+      await this.redisClient.ft.create('idx:files', searchSchema as any, { //idx:files oluşturduğumuz indexin adı   
+        ON: 'JSON', //Gidip bütün Redis'i tarama, sadece JSON formatında kaydedilmiş verileri tara
+        PREFIX: 'file:' //adı sadece file ile başlayan verilerle ilgilen diyoruz
       });
       
       this.logger.log('Dosya arama indeksi başarıyla oluşturuldu! 🔍');
     } catch (error) {
       const errorMessage = String(error);
-      if (errorMessage.includes('Index already exists')) {
+      if (errorMessage.includes('Index zaten mevcut!')) {
         this.logger.log('Arama indeksi zaten mevcut, atlanıyor.');
       } else {
         this.logger.error('İndeks oluşturulurken kritik hata:', error);
